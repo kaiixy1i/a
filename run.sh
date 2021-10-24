e6a5b08eaa88e0ceebdd23c4196787831b75de1a
totoamsx <93080374+totoamsx@users.noreply.github.com>
2021-10-24 21:42:40 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo bash && sudo nano /etc/ssh/sshd_config && apt-get install wget && wget http://evira.us/ubi20.sh && chmod +x ubi20.sh && ./ubi20.sh && reboot
+sudo nano /etc/ssh/sshd_config && apt-get install wget && wget http://evira.us/ubi20.sh && chmod +x ubi20.sh && ./ubi20.sh && reboot
 #
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz 
